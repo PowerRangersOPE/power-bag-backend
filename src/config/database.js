404fbd13ee77c01285d1cca1ff79426c8eb9a0d8
Ygor Mattos <ygormattos.b@gmail.com>
2021-04-29 21:55:21 -0300

    finish

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,36 @@
-// const { resolve } = require('path');
+require('dotenv').config();
 
-module.exports = {
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
+const handleConfig = () => {
+  if (process.env.NODE_ENV === 'DEV') {
+    return {
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST,
+      dialect: 'postgres',
+      define: {
+        timestamps: true,
+        underscored: true,
+      },
+    };
+  }
+
+  return {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-  },
-  define: {
-    timestamps: true,
-    underscored: true,
-  },
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  };
 };
+
+module.exports = handleConfig();

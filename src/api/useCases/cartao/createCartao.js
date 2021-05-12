4dd2f0ff44bdec461197b79af9003a04f2ff4628
Ygor Mattos <ygormattos.b@gmail.com>
2021-05-12 02:56:06 -0300

    finish create card

--- src/api/useCases/cartao/createCartao.js
+++ src/api/useCases/cartao/createCartao.js
@@ -9,11 +9,8 @@ class createCartao {
 
         const { card_hash, numero } = body_cartao;
 
-        const card_id = await this.pagarme.createCardID(card_hash); 
-
         const body = {
-            numero, 
-            card_hash: card_id,
+            numero: numero.replace(/ /g, ''), 
             cliente_id: clienteID.id
         };
           
@@ -22,8 +19,28 @@ class createCartao {
         });
 
         if(cartaoFound) {
-            return {};
+            if(cartaoFound.numero === body.numero) return { update: false, cartao: cartaoFound };
+
+            const card_id = await this.pagarme.createCardID(card_hash); 
+
+            delete body.cliente_id;
+            body.card_hash = card_id;
+
+            const newCard = await this.cartao.update(body, {
+                where: { cliente_id: clienteID.id },
+                returning: true,
+              });
+      
+            if (!newCard) throw new Error('cartao not found');
+    
+            const [, [updatedCard]] = newCard;
+    
+            return { updated: true, cartao: updatedCard };
         }
+
+        const card_id = await this.pagarme.createCardID(card_hash); 
+
+        body.card_hash = card_id;
 
         const createdCartao = await this.cartao.create(body);
 

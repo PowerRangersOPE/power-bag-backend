ff0e4d0f765fadb4db5c3ce690774a7f478dc6da
Ygor Mattos <ygormattos.b@gmail.com>
2021-05-23 12:28:07 -0300

    return nome and email with bag

--- src/api/useCases/bag/findAllBags.js
+++ src/api/useCases/bag/findAllBags.js
@@ -5,7 +5,30 @@ class findAllBags {
 
   async execute() {
     try {
-      const bags = await this.bag.findAll();
+      const bags = await this.bag.findAll({
+        include: [
+          {
+            association: 'cliente',
+            attributes: {
+              exclude: [
+                'id',
+                'senha',
+                'cpf',
+                'identificacao',
+                'tel_cel1',
+                'tel_cel2',
+                'dat_nasc',
+                'status',
+                'pontuacao',
+                'adm',
+                'createdAt',
+                'updatedAt',
+              ],
+            },
+          },
+        ],
+        attributes: { exclude: ['updatedAt', 'transaction_id'] },
+      });
 
       if (!bags) throw new Error('Bags not found');
 

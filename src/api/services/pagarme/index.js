ab0bca2da359a3838e880133ac8cd3a5f789858c
Ygor Mattos <ygormattos.b@gmail.com>
2021-05-23 20:53:23 -0300

    create refund functions

--- src/api/services/pagarme/index.js
+++ src/api/services/pagarme/index.js
@@ -7,6 +7,7 @@ class pagarme {
     this.baseURL = 'https://api.pagar.me/1';
     this.cardURI = '/cards';
     this.transactionURI = '/transactions';
+    this.refundURI = '/refund';
   }
 
   async createCardID(card_hash) {
@@ -47,6 +48,55 @@ class pagarme {
       );
 
       return id;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async makeRefund({ transaction_id, amount }) {
+    try {
+      const url = `${this.baseURL}${this.transactionURI}/${transaction_id}/${this.refundURI}`;
+      const body = {
+        amount,
+        api_key: this.api_key,
+      };
+      const header = {
+        'content-type': 'application/json',
+      };
+
+      const {
+        data: { status },
+      } = await this.request.post(url, body, header);
+
+      return status;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async makeTotalRefund({ transaction_id }) {
+    try {
+      const status = await this.makeRefund({ transaction_id, amount: null });
+
+      if (status !== 'refunded') {
+        throw new Error('pagarme: Ocorreu erro durante o estorno total');
+      }
+
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async makePartialRefund({ transaction_id, amount }) {
+    try {
+      const status = await this.makeRefund({ transaction_id, amount });
+
+      if (status !== 'paid') {
+        throw new Error('pagarme: Ocorreu erro durante o estorno total');
+      }
+
+      return true;
     } catch (error) {
       throw error;
     }

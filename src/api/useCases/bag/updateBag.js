77cfaaf41441607a9c68d953c025588ce5fa6be9
Ygor Mattos <ygormattos.b@gmail.com>
2021-05-23 22:45:17 -0300

    canceled bag

--- src/api/useCases/bag/updateBag.js
+++ src/api/useCases/bag/updateBag.js
@@ -1,24 +1,94 @@
 class updateBag {
-  constructor({ modelBag }) {
+  constructor({ modelBag, pagarme }) {
     this.Bag = modelBag;
+    this.pagarme = pagarme;
+  }
+
+  async updateBag({ id, body }) {
+    const updatedBag = await this.Bag.update(body, {
+      where: { id },
+      returning: true,
+    });
+
+    if (!updatedBag) throw new Error('bag or client not found');
+
+    const [, [bag]] = updatedBag;
+
+    return bag;
+  }
+
+  async canceledBag({ id, transaction_id, status }) {
+    try {
+      const totalRefund = await this.pagarme.makeTotalRefund({
+        transaction_id,
+      });
+
+      if (!totalRefund) {
+        throw new Error('pagarme: Ocorreu erro durante o estorno total');
+      }
+
+      const body = {
+        status,
+        valor: 0,
+      };
+
+      const updatedBag = await this.updateBag({ id, body });
+
+      return updatedBag;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async finishedBag() {}
+
+  updateBagOptions({ status }) {
+    const convertedStatus = status.toLowerCase();
+    const options = {
+      cancelado: 'cancelado',
+      finalizado: 'finalizado',
+    };
+
+    return options[convertedStatus] || false;
   }
 
   async execute({ status, valor, id_bag }) {
     try {
-      const newStatus = {
-        status,
-        valor,
-      };
-      const updatedBag = await this.Bag.update(newStatus, {
+      const optionUpdateBag = this.updateBagOptions({ status });
+
+      if (!optionUpdateBag) {
+        const body = {
+          status,
+          valor,
+        };
+
+        const updatedBag = await this.updateBag({ id: id_bag, body });
+
+        return updatedBag;
+      }
+
+      const data = await this.Bag.findOne({
         where: { id: id_bag },
         returning: true,
       });
 
-      if (!updatedBag) throw new Error('bag or client not found');
+      const { transaction_id, valor_original, id } = data.toJSON();
 
-      const [, [bag]] = updatedBag;
+      const updateBody = {
+        status,
+        valor,
+        transaction_id,
+        valor_original,
+        id,
+      };
 
-      return bag;
+      let updatedBag;
+
+      if (optionUpdateBag === 'cancelado') {
+        updatedBag = await this.canceledBag(updateBody);
+      }
+
+      return updatedBag;
     } catch (error) {
       throw error;
     }
